2f42bac3d17bc4e4b131f460c9b294f7584e85d9
mattfro333 <mattfro333@gmail.com>
2017-04-10 19:59:54 -0600

    hideshow onClick

--- public/controllers/heroCtrl.js
+++ public/controllers/heroCtrl.js
@@ -1,8 +1,9 @@
 angular.module('heroApp').controller('heroCtrl', function($scope, heroSrvc){
 
-
+$scope.showHide = false;
 
 $scope.getHeroes = function(){
+  $scope.showHide = !$scope.showHide;
   heroSrvc.getHeroes().then(response => {
     $scope.heroes = response
   })
