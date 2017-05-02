dd56a4462def92c40cfce8a26451476f509272ba
mattfro333 <mattfro333@gmail.com>
2017-05-02 17:07:05 -0600

    Added Views.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-angular.module('heroApp',[])
+angular.module('heroApp',['ui.router']).config(function($stateProvider, $urlRouterProvider){
+
+            $stateProvider
+            .state('home', {
+              url: '/',
+              templateUrl: '../views/home.html',
+              controller: 'heroCtrl'
+            })
+            .state('birth', {
+              url: '/birth',
+              templateUrl: '../views/heroCreate.html',
+              controller: 'heroCtrl'
+            })
+            $urlRouterProvider.otherwise('/')
+});
