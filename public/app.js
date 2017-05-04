9e18f520e4d8255a396d3fae4e65e13f1ea9b2b4
mattfro333 <mattfro333@gmail.com>
2017-05-03 19:56:34 -0600

    Added Routing

--- public/app.js
+++ public/app.js
@@ -1,15 +1,26 @@
-angular.module('heroApp',['ui.router']).config(function($stateProvider, $urlRouterProvider){
+angular.module('heroApp',['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider){
 
-            $stateProvider
-            .state('home', {
-              url: '/',
-              templateUrl: '../views/home.html',
-              controller: 'heroCtrl'
-            })
-            .state('birth', {
-              url: '/birth',
-              templateUrl: '../views/heroCreate.html',
-              controller: 'heroCtrl'
-            })
-            $urlRouterProvider.otherwise('/')
-});
+      $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: './views/home.html',
+        controller: 'heroCtrl'
+      })
+      .state('birth', {
+        url: '/birth',
+        templateUrl: './views/heroCreate.html',
+        controller: 'heroCtrl'
+      })
+      .state('select', {
+        url: '/select',
+        templateUrl: './views/heroSelect.html',
+        controller: 'heroCtrl'
+      })
+      .state('death', {
+        url: '/death',
+        templateUrl: './views/heroDeath.html',
+        controller: 'heroCtrl'
+      })
+      $urlRouterProvider.otherwise('/')
+  })
