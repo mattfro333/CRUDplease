3150028242a8b6bafe8cee4c46ed9479b9a90262
mattfro333 <mattfro333@gmail.com>
2017-06-29 20:46:57 -0600

    Buddy

--- db/init/createHeroTable.sql
+++ db/init/createHeroTable.sql
@@ -1,5 +1,5 @@
 /* Create Table It does Not Exist */
-DROP TABLE hero;
+-- DROP TABLE hero;
 
 CREATE TABLE IF NOT EXISTS hero(
   Id SERIAL PRIMARY KEY,
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS hero(
   imageurl VARCHAR(400)
 );
 
-INSERT INTO hero(name, origin, hp, imageurl)
-VALUES
-('Wolverine','Funky Genes', 100,'https://s-media-cache-ak0.pinimg.com/736x/d5/51/01/d551017465a490bba582de2c63ae3a14.jpg'),
-('Rogue',	'Funky Genes/ Stole Ms. Marvels Powers',	150,	'http://pa1.narvii.com/5705/9c2b27b08982b6bbd6990b7b69847f036896d8d8_hq.gif'),
-('Simba', 'Africa', 30, 'http://madisoncountycourier.com/wp-content/uploads/2013/03/Barrett-Simba-black-cat.jpg')
+-- INSERT INTO hero(name, origin, hp, imageurl)
+-- VALUES
+-- ('Wolverine','Funky Genes', 100,'https://s-media-cache-ak0.pinimg.com/736x/d5/51/01/d551017465a490bba582de2c63ae3a14.jpg'),
+-- ('Rogue',	'Funky Genes/ Stole Ms. Marvels Powers',	150,	'http://pa1.narvii.com/5705/9c2b27b08982b6bbd6990b7b69847f036896d8d8_hq.gif'),
+-- ('Simba', 'Africa', 30, 'http://madisoncountycourier.com/wp-content/uploads/2013/03/Barrett-Simba-black-cat.jpg')
--- db/init/createPowersTable.sql
+++ db/init/createPowersTable.sql
@@ -1,4 +1,4 @@
-DROP TABLE powers;
+-- DROP TABLE powers;
 
 CREATE TABLE IF NOT EXISTS powers(
   Id SERIAL PRIMARY KEY,
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS powers(
   basicDamage INTEGER
 );
 
-INSERT INTO powers(ultimate, ultDamage, basic, basicDamage)
-VALUES
-('Claw Fury', 20, 'Claw Swipe', 5),
-('Drain Life', 30, 'Punch', 5),
-('Spraying', 10, 'Meowing', 5)
+-- INSERT INTO powers(ultimate, ultDamage, basic, basicDamage)
+-- VALUES
+-- ('Claw Fury', 20, 'Claw Swipe', 5),
+-- ('Drain Life', 30, 'Punch', 5),
+-- ('Spraying', 10, 'Meowing', 5)
